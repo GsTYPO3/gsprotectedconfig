c4a0c510b4933648dc6475ecd1c48e7debc115f8
Gilbertsoft <it@gilbertsoft.ch>
2017-03-14 04:15:54 +0100

    Dummy setup class to prevent errors on update

---
+++ Classes/Setup.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the GS Proteced Config TYPO3 Extension.
+ *
+ * Copyright (C) 2017 by Gilbertsoft (gilbertsoft.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For the full license information, please read the LICENSE file that
+ * was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Gilbertsoft\ProtectedConfig;
+
+
+/**
+ * GS Proteced Config Setup dummy class.
+ */
+class Setup
+{
+	public function afterInstall($extensionKey)
+	{
+	}
+
+	public function afterUninstall($extensionKey)
+	{
+	}
+}
