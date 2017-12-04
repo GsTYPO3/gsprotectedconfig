3960f2e2830d058dcece03c936c8be19ba8c258d
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2017-12-04 10:41:31 +0100

    Usage of Configurator

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,4 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-if (TYPO3_MODE === 'BE') {
-	$signalSlot = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Gilbertsoft\ProtectedConfig\Service\InstallService::class, $_EXTKEY);
-	$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-	$signalSlotDispatcher->connect(
-		\TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
-		'afterExtensionInstall',
-		$signalSlot,
-		'afterInstall'
-	);
-	$signalSlotDispatcher->connect(
-		\TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
-		'afterExtensionUninstall',
-		$signalSlot,
-		'afterUninstall'
-	);
-}
+\Gilbertsoft\ProtectedConfig\Extension\Configurator::localconf($_EXTKEY);
