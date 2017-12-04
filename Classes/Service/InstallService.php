8dc9f4f926847ec484c66d6741327d21f8ef5625
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2017-12-04 10:41:05 +0100

    Derivation from AbstractInstallService

--- Classes/Service/InstallService.php
+++ Classes/Service/InstallService.php
@@ -1,7 +1,8 @@
 <?php
+namespace Gilbertsoft\ProtectedConfig\Service;
 
 /*
- * This file is part of the GS Proteced Config TYPO3 Extension.
+ * This file is part of the "GS Protected Config" Extension for TYPO3 CMS.
  *
  * Copyright (C) 2017 by Gilbertsoft (gilbertsoft.org)
  *
@@ -21,173 +22,127 @@
  * The TYPO3 project - inspiring people to share!
  */
 
-namespace Gilbertsoft\ProtectedConfig\Service;
-
-
-use Gilbertsoft\ProtectedConfig\Utility;
+/**
+ * Use declarations
+ */
+use Gilbertsoft\Lib\Service\AbstractInstallService;
+use Gilbertsoft\Lib\Utility\ObjectUtility;
 use TYPO3\CMS\Core\Configuration\ConfigurationManager;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 
-
 /**
  * GS Proteced Config Install Service class.
  */
-class InstallService
+class InstallService extends AbstractInstallService
 {
-	/**
-	 * @var string Extension key to listen on signals
-	 */
-	protected $extensionKey = 'gsprotectedconfig';
-
     /**
      * @var \TYPO3\CMS\Core\Configuration\ConfigurationManager
      */
     protected $configurationManager = null;
 
     /**
-     * @var string
-     */
-    protected $messageQueueByIdentifier = '';
-
-	/**
-	 * Initializes the install service
-	 *
-	 * @param string $extensionKey Extension key to listen on signals
-	 */
-	public function __construct($extensionKey)
-	{
-		$this->extensionKey = $extensionKey;
-        $this->messageQueueByIdentifier = 'extbase.flashmessages.tx_extensionmanager_tools_extensionmanagerextensionmanager';
-	}
-
-	/**
-	 * Executes the setup tasks if extension is installed.
-	 *
-	 * @param string $extensionKey Installed extension name
-	 */
-	public function afterInstall($extensionKey)
-	{
-        if ($extensionKey == $this->extensionKey)
-        {
-			$this->updateAdditionalConfiguration($extensionKey);
-		}
-	}
-
-	/**
-	 * Executes the setup tasks if extension is uninstalled.
-	 *
-	 * @param string $extensionKey Uninstalled extension name
-	 */
-	public function afterUninstall($extensionKey)
-	{
-        if ($extensionKey == $this->extensionKey)
-        {
-			$this->removeAdditionalConfiguration($extensionKey);
-		}
-	}
-
-	/**
-	 * Get the ConfigurationManager
+     * Get the ConfigurationManager
      *
      * @return \TYPO3\CMS\Core\Configuration\ConfigurationManager
-	 */
-	protected function getConfigurationManager()
-	{
-		if (!isset($this->configurationManager)) {
-			$this->configurationManager = GeneralUtility::makeInstance(ObjectManager::class)
-				->get(ConfigurationManager::class);
-		}
-		return $this->configurationManager;
-	}
+     */
+    protected function getConfigurationManager()
+    {
+        if (!isset($this->configurationManager)) {
+            $this->configurationManager = ObjectUtility::getConfigurationManager();
+        }
 
-	/**
-	 * Returns the lines from AdditionalConfiguration.php file without own additions
-	 */
-	protected function getCleanAdditionalConfiguration($extensionKey)
-	{
-		$newLines = [];
+        return $this->configurationManager;
+    }
 
-		// Load content and search for the include
-		if (($content = GeneralUtility::getUrl($this->getConfigurationManager()->getAdditionalConfigurationFileLocation())) !== false)
-		{
-			$currentLines = explode(LF, $content);
+    /**
+     * Returns the lines from AdditionalConfiguration.php file without own additions
+     *
+     * @param string $extensionKey Extension key
+     */
+    protected function getCleanAdditionalConfiguration($extensionKey)
+    {
+        $newLines = [];
 
-			// Delete the php marker line
-			array_shift($currentLines);
+        // Load content and search for the include
+        if (($content = GeneralUtility::getUrl($this->getConfigurationManager()->getAdditionalConfigurationFileLocation())) !== false)
+        {
+            $currentLines = explode(LF, $content);
 
-			//
-			$startFound = false;
-			$endFound = false;
+            // Delete the php marker line
+            array_shift($currentLines);
 
-			foreach ($currentLines as $line)
-			{
-				if (($startFound) && (!$endFound)) {
-					$endFound = (strpos($line, '}') !== false);
-				} elseif ((!$startFound) && (strpos($line, $extensionKey) !== false)) {
-					$startFound = true;
-					$endFound = false;
-				} else {
-					$newLines[] = $line;
-				}
-			}
-		}
+            //
+            $startFound = false;
+            $endFound = false;
 
-		// Remove blank lines at the end
-		$revLines = array_reverse($newLines);
-		$newLines = [];
-		$lineFound = false;
+            foreach ($currentLines as $line)
+            {
+                if (($startFound) && (!$endFound)) {
+                    $endFound = (strpos($line, '}') !== false);
+                } elseif ((!$startFound) && (strpos($line, $extensionKey) !== false)) {
+                    $startFound = true;
+                    $endFound = false;
+                } else {
+                    $newLines[] = $line;
+                }
+            }
+        }
 
-		foreach ($revLines as $line)
-		{
-			if ($lineFound || !empty(trim($line))) {
-				$newLines[] = $line;
-				$lineFound = true;
-			}
-		}
+        // Remove blank lines at the end
+        $revLines = array_reverse($newLines);
+        $newLines = [];
+        $lineFound = false;
 
-		return array_reverse($newLines);
-	}
+        foreach ($revLines as $line)
+        {
+            if ($lineFound || !empty(trim($line))) {
+                $newLines[] = $line;
+                $lineFound = true;
+            }
+        }
 
-	/**
-	 * Creates the AdditionalConfiguration.php file with necessary includes
-	 */
-	protected function updateAdditionalConfiguration($extensionKey)
-	{
+        return array_reverse($newLines);
+    }
+
+    /**
+     * Creates the AdditionalConfiguration.php file with necessary includes
+     *
+     * @param string $extensionKey Extension key
+     */
+    protected function updateAdditionalConfiguration($extensionKey)
+    {
         /**
          * Add Flashmessage that the configuration has to be checked
          */
-        $flashMessage = GeneralUtility::makeInstance(
-            'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+        $this->showFlashMessage(
             'To enable the features you have to edit the extensions configuration in the Extension Manager. After the new configuration is saved please relogin to the backend to ensure the changes!',
             'Check your configuration',
             FlashMessage::NOTICE,
             true
         );
-        $this->addFlashMessage($flashMessage);
 
         /**
          * Update configuration
          */
-		$newLines = $this->getCleanAdditionalConfiguration($extensionKey);
+        $newLines = $this->getCleanAdditionalConfiguration($extensionKey);
 
-		if (!empty(end($newLines))) {
-			$newLines[] = '';
-		}
+        if (!empty(end($newLines))) {
+            $newLines[] = '';
+        }
 
-		$newLines[] = '// Run configuration modifier for extension ' . $extensionKey . ' - added on ' . date(DATE_ATOM) . ' by ' . __CLASS__;
-		$newLines[] = 'if (class_exists(\'Gilbertsoft\ProtectedConfig\Configuration\Modifier\')) {';
-		$newLines[] = '	\Gilbertsoft\ProtectedConfig\Configuration\Modifier::processLocalConfiguration(\'' . $extensionKey . '\');';
-		$newLines[] = '}';
+        $newLines[] = '// Run configuration modifier for extension ' . $extensionKey . ' - added on ' . date(DATE_ATOM) . ' by ' . __CLASS__;
+        $newLines[] = 'if (class_exists(\'Gilbertsoft\ProtectedConfig\Extension\Configurator\')) {';
+        $newLines[] = '    \Gilbertsoft\ProtectedConfig\Extension\Configurator::additionalConfiguration(\'' . $extensionKey . '\');';
+        $newLines[] = '}';
 
-		$this->getConfigurationManager()->writeAdditionalConfiguration($newLines);
+        $this->getConfigurationManager()->writeAdditionalConfiguration($newLines);
 
         /**
          * Add Flashmessage that the configuration has written
          */
-        $flashMessage = GeneralUtility::makeInstance(
-            'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+        $this->showFlashMessage(
             'Configuration successfully added to ' .
             $this->getConfigurationManager()->getAdditionalConfigurationFileLocation() .
             '.',
@@ -195,67 +150,72 @@ class InstallService
             FlashMessage::OK,
             true
         );
-        $this->addFlashMessage($flashMessage);
-	}
-
-	/**
-	 * Removes the added lines in the AdditionalConfiguration.php file
-	 */
-	protected function removeAdditionalConfiguration($extensionKey)
-	{
-		$newLines = $this->getCleanAdditionalConfiguration($extensionKey);
-
-		if (empty($newLines) !== true)
-		{
-			$this->getConfigurationManager()->writeAdditionalConfiguration($newLines);
-
-			/**
-			 * Add Flashmessage that the configuration has written
-			 */
-			$flashMessage = GeneralUtility::makeInstance(
-				'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
-				'Configuration successfully removed from ' .
-				$this->getConfigurationManager()->getAdditionalConfigurationFileLocation() .
-				'.',
-				'Configuration removed',
-				FlashMessage::OK,
-				true
-			);
-			$this->addFlashMessage($flashMessage);
-		}
-		else
-		{
-			unlink($this->getConfigurationManager()->getAdditionalConfigurationFileLocation());
-
-			/**
-			 * Add Flashmessage that the configuration has deleted
-			 */
-			$flashMessage = GeneralUtility::makeInstance(
-				'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
-				'File ' .
-				$this->getConfigurationManager()->getAdditionalConfigurationFileLocation() .
-				' successfully deleted.',
-				'Configuration delete',
-				FlashMessage::OK,
-				true
-			);
-			$this->addFlashMessage($flashMessage);
-		}
-	}
+    }
 
     /**
-     * Adds a Flash Message to the Flash Message Queue
+     * Removes the added lines in the AdditionalConfiguration.php file
      *
-     * @param FlashMessage $flashMessage
+     * @param string $extensionKey Extension key
      */
-    protected function addFlashMessage(FlashMessage $flashMessage)
+    protected function removeAdditionalConfiguration($extensionKey)
     {
-        if ($flashMessage) {
-            /** @var $flashMessageService \TYPO3\CMS\Core\Messaging\FlashMessageService */
-            $flashMessageService = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessageService');
-            /** @var $flashMessageQueue \TYPO3\CMS\Core\Messaging\FlashMessageQueue */
-            $flashMessageQueue = $flashMessageService->getMessageQueueByIdentifier($this->messageQueueByIdentifier);
-            $flashMessageQueue->enqueue($flashMessage);
+        $newLines = $this->getCleanAdditionalConfiguration($extensionKey);
+
+        if (empty($newLines) !== true)
+        {
+            $this->getConfigurationManager()->writeAdditionalConfiguration($newLines);
+
+            /**
+             * Add Flashmessage that the configuration has written
+             */
+            $this->showFlashMessage(
+                'Configuration successfully removed from ' .
+                $this->getConfigurationManager()->getAdditionalConfigurationFileLocation() .
+                '.',
+                'Configuration removed',
+                FlashMessage::OK,
+                true
+            );
+        }
+        else
+        {
+            unlink($this->getConfigurationManager()->getAdditionalConfigurationFileLocation());
+
+            /**
+             * Add Flashmessage that the configuration has deleted
+             */
+            $this->showFlashMessage(
+                'File ' . $this->getConfigurationManager()->getAdditionalConfigurationFileLocation() . ' successfully deleted.',
+                'Configuration delete',
+                FlashMessage::OK,
+                true
+            );
+        }
+    }
+
+    /**
+     * Executes the setup tasks if extension is installed.
+     *
+     * @param string $extensionKey Installed extension name
+     */
+    public function afterInstall($extensionKey)
+    {
+        if ($extensionKey == $this->extensionKey)
+        {
+            $this->updateAdditionalConfiguration($extensionKey);
+        }
+    }
+
+    /**
+     * Executes the setup tasks if extension is uninstalled.
+     *
+     * @param string $extensionKey Uninstalled extension name
+     */
+    public function afterUninstall($extensionKey)
+    {
+        if ($extensionKey == $this->extensionKey)
+        {
+            $this->removeAdditionalConfiguration($extensionKey);
         }
     }
 }
